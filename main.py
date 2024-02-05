cd1b28e24a4d0d8b6108fc3c5d1cf6354df4aaa7
DrFaustest <scottaf80234@gmail.com>
2024-02-05 12:49:37 -0600

    Fix bug in login functionality

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 import pygame as pg
-from game import Game
+from game.game import Game
 from settings import *
 from typing import *
 
-'''
-This module is the main function to run Brick Breaker. It initializes the game and runs the game loop, sets up the game window, and controls the game loop. the game has three states: start, playing, and game over. The game starts in the start state, where the player can click the play button to start the game. The game then moves to the playing state, where the player can move the paddle and hit the ball to break the bricks. The game ends when the player clears all the bricks or the ball hits the bottom of the screen. The game then moves to the game over state, where the player can click the play button to restart the game.
-
-Classes: 
-    Game: Handles the game logic, and updates, and rendering.
-
-Constants:
-    SCREEN_WIDTH: The width of the game window as determined in the settings module.
-    SCREEN_HEIGHT: The height of the game window as determined in the settings module.
-    FPS: The frame rate of the game as determined in the settings module.
-
-Functions:
-    main(): Initializes the game and runs the game loop and processes user input.
-'''
 
 def main() -> None:
     pg.init()
