25ef793c958c8c4a7c6b480f96b6bb2df21cf655
DrFaustest <scottaf80234@gmail.com>
2024-02-05 20:26:38 -0600

    Add Brick class to the game

---
+++ objects/brick.py
@@ -0,0 +1,16 @@
+import pygame as pg
+from settings import *
+
+class Brick:
+    image = pg.image.load("img/brick_img.png")
+    image = pg.transform.scale(image, (BRICK_WIDTH, BRICK_HEIGHT))
+    
+    def __init__(self, x, y):
+        self.rect = pg.Rect(x, y, BRICK_WIDTH, BRICK_HEIGHT)
+        self.is_destroyed = False  # A flag to track if the brick is destroyed
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def destroy(self):
+        self.is_destroyed = True
