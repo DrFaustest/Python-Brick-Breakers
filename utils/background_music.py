4d861912c0ee254ed84b9cfb51f8d533dd6f47c6
DrFaustest <scottaf80234@gmail.com>
2024-02-16 19:55:33 -0600

    Fix background music playback and track index

--- utils/background_music.py
+++ utils/background_music.py
@@ -8,8 +8,9 @@ class BackgroundMusic:
         self.enabled_image = pg.transform.scale(self.enabled_image, (50, 50))
         self.disabled_image: pg.Surface = pg.image.load(SOUND_DISABLED_IMAGE)
         self.disabled_image = pg.transform.scale(self.disabled_image, (50, 50))
+        self.current_image = self.enabled_image
         self.playlist = ["sound/background_1.mp3", "sound/background_2.mp3", "sound/background_1.mid", "sound/background_2.mid"]
-        self.current_track_index = 0
+        self.current_track_index = 3
         pg.mixer.music.set_volume(VOLUME)
         pg.mixer.music.set_endevent(pg.USEREVENT)
         self.play()
@@ -20,19 +21,19 @@ class BackgroundMusic:
         else:
             self.play()
 
-    def play(self) -> None:
-        # Load the initial track based on current index
+    def play(self):
+        if not self.enabled:
+            self.enabled = True
+            self.current_image = self.enabled_image
         pg.mixer.music.load(self.playlist[self.current_track_index])
-        pg.mixer.music.play(-1)  # Play indefinitely
-        self.enabled = True
-        self.current_image = self.enabled_image
+        pg.mixer.music.play(0)
 
     def play_next_track(self):
+        if not self.enabled:
+            return
         self.current_track_index = (self.current_track_index + 1) % len(self.playlist)
         pg.mixer.music.load(self.playlist[self.current_track_index])
-        pg.mixer.music.play(-1)  # Play indefinitely
-        self.enabled = True
-        self.current_image = self.enabled_image
+        pg.mixer.music.play(0)
 
     def stop(self) -> None:
         pg.mixer.music.stop()
