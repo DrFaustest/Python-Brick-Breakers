86812b63916f36417c1473ff415a5cb7d2a9a0f0
DrFaustest <scottaf80234@gmail.com>
2024-02-16 20:26:15 -0600

    Add logo display before game starts

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame as pg
 from game.game import Game
 from settings import *
-from utils import background_music
+from logo import LogoDisplay
 
 def main() -> None:
     pg.init()
     screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pg.display.set_caption("Brick Breaker")
     game = Game(screen)
+    logo_display = LogoDisplay(screen)
     running = True
     clock = pg.time.Clock()
     while running:
@@ -17,8 +18,14 @@ def main() -> None:
                 game.background_music.play_next_track()
             if event.type == pg.QUIT:
                 running = False
-        game.update(events)  # Pass the events to the game update method
-        game.draw()
+        if not logo_display.done:
+            logo_display.update()
+            logo_display.draw()
+            pg.display.flip()
+            continue
+        else:
+            game.update(events)
+            game.draw()
         pg.display.flip()
         clock.tick(FPS)
     pg.quit()
