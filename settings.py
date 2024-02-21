7c6e3fd9f39da3f6cbf2945bad435e0a0c441bc6
DrFaustest <scottaf80234@gmail.com>
2024-02-21 08:51:12 -0600

    Update game settings and dimensions

--- settings.py
+++ settings.py
@@ -3,23 +3,25 @@ import pygame as pg
 import math
 
 # Screen dimensions
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-BRICK_WIDTH = 100
-BRICK_HEIGHT = 20
+SCALE = 1
+SCREEN_WIDTH = 800 * SCALE
+SCREEN_HEIGHT = 600 * SCALE
+BRICK_SIZE = (100 * SCALE, 20 * SCALE)
+
 
 # Key bindings
-KEY_MOVE_LEFT = pg.K_LEFT  # Update with the actual key code or name
-KEY_MOVE_RIGHT = pg.K_RIGHT  # Update with the actual key code or name
-KEY_PAUSE = pg.K_p  # Update with the actual key code or name
-KEY_QUIT = pg.K_DELETE  # Update with the actual key code or name
+KEY_MOVE_LEFT = pg.K_LEFT 
+KEY_MOVE_RIGHT = pg.K_RIGHT  
+KEY_PAUSE = pg.K_p 
+KEY_QUIT = pg.K_DELETE 
 
 # Game settings+
 FPS = 120  # Frames per second
-PADDLE_SPEED = 25  # Update with the actual speed value
-BALL_SPEED = 5 # Update with the actual speed value
-DIFFICULTY = 1  # Update with the changeable difficulty level
-VOLUME = .5  # Update with the actual volume level
+PADDLE_SPEED = 25
+PADDLE_SIZE = (100 * SCALE, 20 * SCALE)
+BALL_SPEED = 5
+DIFFICULTY = 1  
+VOLUME = .5 
 # Colors (RGB)
 WHITE = (255, 255, 255)
 GRAY = (128, 128, 128)
@@ -30,7 +32,7 @@ BLACK = (0, 0, 0)
 
 MAX_REFLECTION_ANGLE = 90
 MIN_Y_VELOCITY = -1.5
-BALL_RADIUS = 15  # Update with the actual radius value
+BALL_RADIUS = 30 * SCALE / 2
 
 BALL_IMG = "img/retro_snow_ball.png"
 PADDLE_IMG = "img/paddle.png"
