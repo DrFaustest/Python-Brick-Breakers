371f78db3e59d460090b7d72147edef1f588eb8a
DrFaustest <scottaf80234@gmail.com>
2024-02-25 12:46:36 -0600

    Refactor Scoreboard class initialization and score manipulation methods

--- ui/scoreboard.py
+++ ui/scoreboard.py
@@ -2,26 +2,44 @@ import pygame as pg
 from settings import *
 
 class Scoreboard:
-    def __init__(self, font_size=30, color=(255, 255, 255)):
+    def __init__(self, font_size=30, color=WHITE):
+        """
+        Initialize the Scoreboard object.
+
+        Args:
+            font_size (int): The font size of the score text. Default is 30.
+            color (tuple): The color of the score text. Default is WHITE.
+        """
         self.score = 0
         self.font = pg.font.SysFont(None, font_size)
         self.color = color
-        self.x = SCREEN_WIDTH - 100
+        self.x = SCREEN_WIDTH - 120
         self.y = SCREEN_HEIGHT - 25
 
     def increase_score(self, points=10):
-        # Increase the score by the given number of points
+        """
+        Increase the score by the specified number of points.
+
+        Args:
+            points (int): The number of points to increase the score by. Default is 10.
+        """
         self.score += points
 
     def decrease_score(self, points=10):
-        # Decrease the score by the given number of points
+        """
+        Decrease the score by the specified number of points.
+
+        Args:
+            points (int): The number of points to decrease the score by. Default is 10.
+        """
         self.score -= points
 
-    def update(self):
-        # This method can be used to update any other information on the scoreboard if needed
-        pass
-
     def draw(self, screen):
-        # Render the score and draw it on the screen
+        """
+        Draw the score on the screen.
+
+        Args:
+            screen (pygame.Surface): The surface to draw the score on.
+        """
         score_text = self.font.render(f'Score: {self.score}', True, self.color)
         screen.blit(score_text, (self.x, self.y))
