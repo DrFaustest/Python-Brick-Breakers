a6fee759ee05e520219652c97931d0b64c2749c4
DrFaustest <scottaf80234@gmail.com>
2024-03-06 10:48:01 -0600

    Add preview windows for background, ball, and paddle images

--- ui/UI_TEST_FILE.py
+++ ui/UI_TEST_FILE.py
@@ -2,6 +2,12 @@ import pygame
 import sys
 from slider import Slider
 from text_box import TextBox
+from preview_window import PreviewWindow
+from preview_window import PreviewWindow
+
+BACKGROUND_IMAGES = ["img/background.webp", "img/background_2.webp", "img/background_3.webp", "img/background_4.webp"]
+BALL_IMAGES = ["img/future_ball.png","img/retro_snow_ball.png","img/ring_ball.png"]
+PADDLE_IMAGES = ["img/paddle.png"]
 
 # Initialize Pygame
 pygame.init()
@@ -10,19 +16,26 @@ def name_entered(name):
     print(f"Name entered: {name}")
 
 # Set up the display
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((600, 800))
 clock = pygame.time.Clock()
-input_box = TextBox(100, 100, 140, 32, name_entered, "Enter your name: ")
+preview_window_background=PreviewWindow(screen, BACKGROUND_IMAGES,"Backgound" , 60, 10, 200,200)
+preview_window_ball=PreviewWindow(screen, BALL_IMAGES,"Ball", 60, 250, 200,200)
+preview_window_paddle=PreviewWindow(screen, PADDLE_IMAGES,"Paddle",60, 500, 200,100)
 done = False
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        input_box.handle_event(event)
+        else:
+            preview_window_background.handle_event(event)
+            preview_window_ball.handle_event(event)
+            preview_window_paddle.handle_event(event)
+            
+    preview_window_background.draw()
+    preview_window_ball.draw()
+    preview_window_paddle.draw()
 
-    screen.fill((0, 0, 0))  # Clear screen with black
-    input_box.draw(screen)
 
     pygame.display.flip()
     clock.tick(30)
