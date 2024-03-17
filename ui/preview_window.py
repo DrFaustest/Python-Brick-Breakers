b30f1bbfa906795d8cb987e3d39e70d7b52debba
DrFaustest <scottaf80234@gmail.com>
2024-03-17 10:36:10 -0500

    Refactor image loading and caching in PreviewWindow class for lazy loading

--- ui/preview_window.py
+++ ui/preview_window.py
@@ -1,51 +1,38 @@
 import pygame
-from pygame.locals import *
 
 class PreviewWindow:
-    def __init__(self,screen, image_list,image_description, x, y, display_width, display_height):
+    def __init__(self, screen, image_list, image_description, x, y, display_width, display_height):
         self.window = screen
         self.image_list = image_list
         self.image_description = image_description
-        self.index = 0
         self.x = x
         self.y = y
         self.display_width = display_width
         self.display_height = display_height
         self.button_width = 50
         self.button_height = 50
+        self.images = {}  # Cache for loaded images
+        self.current_image = None
+        self.index = 0
         self.unsaved_changes = False
-        self.load_images()
         self.create_buttons()
+        self.load_current_image()  # Load only the current image initially
 
-    def draw(self):
-        self.draw_image()
-        self.draw_buttons()
-
-    def draw_text(self, text, position, font_size=20, text_color=(255, 255, 255), background_color=None):
-        font = pygame.font.Font(None, font_size)  # None uses the default font
-        text_surface = font.render(text, True, text_color, background_color)
-        text_rect = text_surface.get_rect(center=position)
-        self.window.blit(text_surface, text_rect)
-
-
-    def load_images(self):
-        self.images = []
-        self.image_paths = self.image_list  # Store image paths
-        for img_path in self.image_list:
-            image = pygame.image.load(img_path)
+    def load_current_image(self):
+        # Load and scale the current image if it's not already in the cache
+        img_path = self.image_list[self.index]
+        if img_path not in self.images:
+            image = pygame.image.load(img_path).convert_alpha()
             img_width, img_height = image.get_size()
             scale_factor = min((self.display_width - 2 * self.button_width) / img_width, self.display_height / img_height)
             new_size = (int(img_width * scale_factor), int(img_height * scale_factor))
-            self.images.append(pygame.transform.scale(image, new_size))
+            self.images[img_path] = pygame.transform.scale(image, new_size)
+        self.current_image = self.images[img_path]
 
     def create_buttons(self):
         mid_y = self.y + (self.display_height - self.button_height) // 2
         self.left_button = pygame.Rect(self.x - self.button_width, mid_y, self.button_width, self.button_height)
         self.right_button = pygame.Rect(self.x + self.display_width, mid_y, self.button_width, self.button_height)
-        # Define hitboxes for arrows
-        self.left_arrow_hitbox = self.left_button  # This can be more precise if necessary
-        self.right_arrow_hitbox = self.right_button  # This can be more precise if necessary
-
 
     def draw_buttons(self):
         pygame.draw.rect(self.window, (100, 100, 100), self.left_button)
@@ -59,33 +46,36 @@ class PreviewWindow:
         self.window.blit(right_arrow, right_arrow_pos)
 
     def draw_image(self):
-        self.window.fill((255, 255, 255), (self.x, self.y, self.display_width, self.display_height))
-        descriptive_text = self.image_description
-        self.draw_text(descriptive_text, (self.x + self.display_width // 2, self.y + self.display_height + 20))
-        current_image = self.images[self.index]
-        image_width, image_height = current_image.get_size()
-        scale_factor_w = self.display_width / image_width
-        scale_factor_h = self.display_height / image_height
-        scale_factor = min(scale_factor_w, scale_factor_h)
-        new_image_width = int(image_width * scale_factor)
-        new_image_height = int(image_height * scale_factor)
-        image_x = self.x + (self.display_width - new_image_width) // 2
-        image_y = self.y + (self.display_height - new_image_height) // 2
-        self.window.blit(pygame.transform.scale(current_image, (new_image_width, new_image_height)), (image_x, image_y))
-        self.draw_buttons()
+        if self.current_image:
+            self.window.fill((0, 0, 0), (self.x, self.y, self.display_width, self.display_height))
+            image_width, image_height = self.current_image.get_size()
+            image_x = self.x + (self.display_width - image_width) // 2
+            image_y = self.y + (self.display_height - image_height) // 2
+            self.window.blit(self.current_image, (image_x, image_y))
+            self.draw_text(self.image_description, (self.x + self.display_width // 2, self.y + self.display_height + 20))
+
+    def draw_text(self, text, position, font_size=20, text_color=(255, 255, 255), background_color=None):
+        font = pygame.font.Font(None, font_size)
+        text_surface = font.render(text, True, text_color, background_color)
+        text_rect = text_surface.get_rect(center=position)
+        self.window.blit(text_surface, text_rect)
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
-            # Check if the click is on the left arrow
-            if self.left_arrow_hitbox.collidepoint(event.pos):
-                self.index = (self.index - 1) % len(self.images)
+            if self.left_button.collidepoint(event.pos):
+                self.index = (self.index - 1) % len(self.image_list)
+                self.load_current_image()
                 self.draw_image()
                 self.unsaved_changes = True
-            # Check if the click is on the right arrow
-            elif self.right_arrow_hitbox.collidepoint(event.pos):
-                self.index = (self.index + 1) % len(self.images)
+            elif self.right_button.collidepoint(event.pos):
+                self.index = (self.index + 1) % len(self.image_list)
+                self.load_current_image()
                 self.draw_image()
                 self.unsaved_changes = True
 
     def get_value(self):
-        return (self.image_description, self.image_paths[self.index])
+        return (self.image_description, self.image_list[self.index])
+
+    def draw(self):
+        self.draw_image()
+        self.draw_buttons()
